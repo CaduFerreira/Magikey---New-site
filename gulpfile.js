a62d4dff60869054238025a0de0b48964e97fb94
CaduFerreira <caduferreira@MacBook-Air-de-Cadu.local>
2016-07-04 14:41:02 -0300

    First Steps
    trying to put together the home page.

--- gulpfile.js
+++ gulpfile.js
@@ -56,8 +56,10 @@ gulp.task('sass', function () {
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-    gulp.watch('_scss/*.scss', ['sass']);
-    gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
+    gulp.watch('assets/css/**', ['sass']);
+    gulp.watch('assets/js/**', ['jekyll-rebuild']);
+    gulp.watch(['index.html', '_layouts/*.html', '_includes/*'], ['jekyll-rebuild']);
+    gulp.watch(['assets/js/**'], ['jekyll-rebuild']);
 });
 
 /**
